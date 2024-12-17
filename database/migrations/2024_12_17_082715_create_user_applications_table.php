f8555b7611a76577f6012e5cb9e3d746c4ef664e
Michał Marciniak <michal.marciniak.it@gmail.com>
2024-12-17 10:14:04 +0100

    feat: created first model and migrations

--- database/migrations/2024_12_17_082715_create_user_applications_table.php
+++ database/migrations/2024_12_17_082715_create_user_applications_table.php
@@ -10,10 +10,18 @@ return new class () extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_applications', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignId('application_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
             $table->timestamps();
         });
     }
@@ -23,6 +31,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_applications');
     }
 };
