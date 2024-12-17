eb20c5377127e2b319ad56361ca2eb1b5c6224da
Michał Marciniak <michal.marciniak.it@gmail.com>
2024-12-17 12:51:14 +0100

    refactor: changed passport to sanctum trait HasApiTokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
     use HasApiTokens;
+
+    protected $hidden = [
+        'password'
+    ];
+
+    protected function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $password) => Hash::make($password),
+        );
+    }
 }
