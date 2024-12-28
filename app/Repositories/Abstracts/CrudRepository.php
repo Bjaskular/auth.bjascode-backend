42f7b0102706d0a28440833b6ee86e2096de6cbe
Michał Marciniak <michal.marciniak.it@gmail.com>
2024-12-28 14:11:25 +0100

    feat: created general repository for crud actions

---
+++ app/Repositories/Abstracts/CrudRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories\Abstracts;
+
+use App\Repositories\Interfaces\IRepository;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Traits\ForwardsCalls;
+
+abstract class CrudRepository implements IRepository
+{
+    use ForwardsCalls;
+
+    protected Model $model;
+    protected Builder $query;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+        $this->query = $model->query();
+    }
+
+    public function clear(): void
+    {
+        $this->model = new $this->model;
+        $this->query = $this->model->query();
+    }
+
+    public function __call($name, $arguments): mixed
+    {
+        $result = $this->forwardDecoratedCallTo($this->query, $name, $arguments);
+
+        if (! $result instanceof IRepository) {
+            $this->clear();
+        }
+
+        return $result;
+    }
+}
---
+++ app/Repositories/Interfaces/IRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface IRepository
+{
+    public function clear(): void;
+}
