f3e829a97edb4e493c330375fe8b3617900fe920
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-01-09 01:27:01 +0100

    test: implemented first test for user controller

--- tests/Feature/Controllers/UserTest.php
+++ tests/Feature/Controllers/UserTest.php
@@ -2,26 +2,31 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\PersonalAccessToken;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-// use Laravel\Sanctum\Sanctum;
+use Laravel\Sanctum\Sanctum;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 final class UserTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function test_example(): void
+    #[Test]
+    public function example(): void
     {
         /** @var \App\Models\User $user */
         $user = User::factory()->create(['password' => 'zaq1@WSX']);
-        // Sanctum::actingAs($user);
+        Sanctum::actingAs($user);
 
         $response = $this->post(route('api.login'), ['email' => $user->email, 'password' => 'zaq1@WSX'], [
             'Accept' => 'appllication/json'
         ]);
 
-        $response->dump();
-        dd();
+        $cookie = $response->getCookie('AuthBjascode_token', false)->getValue();
+
+        $response->assertNoContent()->assertPlainCookie('AuthBjascode_token');
+        $this->assertEquals($user->id, PersonalAccessToken::findToken($cookie)->tokenable()->first()->id);
     }
 }
