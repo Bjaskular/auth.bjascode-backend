fc218fc23fce40d3fc950c79d05563504f2dcf52
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-02-09 21:06:33 +0100

    feat: implemented refresh endpoint, added new sanctum middlewares

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,18 @@
 <?php
 
+use App\Enums\AuthCookieName;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('login')->post('/login', [AuthController::class, 'login']);
-// Route::post('/refresh', [AuthController::class, 'refreshToken']);
-
 Route::group(['middleware' => ['auth:sanctum']], function () {
     // Route::get('/me', [AuthController::class, 'me']);
-    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/refresh', [AuthController::class, 'refreshToken'])
+        ->middleware('abilities:'. AuthCookieName::REFRESH->value)
+        ->name('refresh_token');
+
+    Route::delete('/logout', [AuthController::class, 'logout'])
+        ->middleware('abilities:'. AuthCookieName::API_ACCESS->value)
+        ->name('logout');
 });
-
-// Route::name('test')->post('/tokens/create', function (Request $request) {
-
-//     /** @var \App\Models\User $user */
-//     $user = $request->user();
-
-//     $token = $user->createToken($request->token_name, ['*'], now()->addSeconds(config('session.lifetime')));
-
-//     return ['token' => $token->plainTextToken];
-// });
