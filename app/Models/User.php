89db4f3e072cd4ed26eeaa3d017dad0ad94b5074
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-05-16 01:24:01 +0200

    feat: added relationship between modules

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use App\Traits\HasUUID;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
@@ -30,5 +32,22 @@ class User extends Authenticatable
     public function getHashEmail(): ?string
     {
         return $this->email ? sha1($this->email) : null;
+    }
+
+    public function applications(): HasManyThrough
+    {
+        return $this->hasManyThrough(Application::class, UserApplication::class);
+    }
+
+    public function application(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            Application::class,
+            UserApplication::class,
+            'user_id',
+            'id',
+            'id',
+            'application_id'
+        );
     }
 }
--- app/Models/UserApplication.php
+++ app/Models/UserApplication.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class UserApplication extends Pivot
@@ -10,4 +11,14 @@ class UserApplication extends Pivot
     use HasFactory;
 
     public $incrementing = true;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function application(): BelongsTo
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
