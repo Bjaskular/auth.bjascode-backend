e80b5cac3158a237c847cd0c7113f1f091ecab94
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-05-16 01:21:02 +0200

    feat: create forbidden exception

---
+++ app/Http/Exceptions/ForbiddenException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Exceptions;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+final class ForbiddenException extends HttpException
+{
+    public function __construct(string $message = '')
+    {
+        parent::__construct(Response::HTTP_FORBIDDEN, $message);
+    }
+}
