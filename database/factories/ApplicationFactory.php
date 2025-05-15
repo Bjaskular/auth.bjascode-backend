62138ca84e4a97070841d73a2b81be8f2c41b2df
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-05-16 01:25:01 +0200

    feat: created new factories

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Application>
+ */
+class ApplicationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'key' => $this->faker->unique()->word(),
+            'url' => $this->faker->url(),
+            'secret' => base64_encode($this->faker->word()),
+        ];
+    }
+}
