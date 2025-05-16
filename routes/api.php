f9f020dc8e8f6e9e195ea8b85c13eb3a0ba34ffb
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-05-16 15:16:28 +0200

    fix: changed abilities in endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
-use App\Enums\AuthCookieName;
+use App\Enums\TokenAbility;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('login')->post('/login', [AuthController::class, 'login']);
+
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/me', [AuthController::class, 'me'])->name('me');
+    Route::get('/me', [AuthController::class, 'me'])
+        ->middleware('abilities:'. TokenAbility::ACCESS_API->value)
+        ->name('me');
 
     Route::get('/refresh', [AuthController::class, 'refreshToken'])
-        ->middleware('abilities:'. AuthCookieName::REFRESH->value)
+        ->middleware('abilities:'. TokenAbility::REFRESH_ACCESS_TOKEN->value)
         ->name('refresh_token');
 
     Route::delete('/logout', [AuthController::class, 'logout'])
-        ->middleware('abilities:'. AuthCookieName::API_ACCESS->value)
+        ->middleware('abilities:'. TokenAbility::ACCESS_API->value)
         ->name('logout');
 });
