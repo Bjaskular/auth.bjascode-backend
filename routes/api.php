99a48841219b1d8986f929a6fa644be29ea48e96
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-05-16 20:04:56 +0200

    style: corrected style code

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Enums\TokenAbility;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('login')->post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/me', [AuthController::class, 'me'])
