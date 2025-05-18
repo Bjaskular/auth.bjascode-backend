1df6736c0aa674fc5536b6965bbc9bdfa5712226
Michał Marciniak <michal.marciniak.it@gmail.com>
2025-05-18 13:45:59 +0200

    refactor: corrected nginx config

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\AngularAppController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/doc/87e1ab37-81a5-434b-a9e0-e5e305c09100', function () {
+Route::get('/docs/87e1ab37-81a5-434b-a9e0-e5e305c09100', function () {
     return view("scribe.index");
 });
-
-Route::get('{any}', [AngularAppController::class, 'index'])
-    ->where('any', '^(?!api|docs|gp|storage)(?!.*\.\w{2,5}$).*');
